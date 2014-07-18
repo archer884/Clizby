c0339932444b99854c9b6079fd1c125f97c0f38f
J/A <archer884@gmail.com>
2014-07-18 14:19:25 -0500

    Upgraded argument processing :)

--- JA.Clizby/Mapper.cs
+++ JA.Clizby/Mapper.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JA.Clizby
 {
---
+++ JA.Clizby/Parameter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace JA.Clizby
+{
+    public class Parameter
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        
+        public Parameter(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public object Read(Type type)
+        {
+            return Read(value => TypeDescriptor.GetConverter(type).ConvertFromString(value), type);
+        }
+
+        public T Read<T>()
+        {
+            return Read(value => (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(value), null);
+        }
+
+        public T Read<T>(Func<string, T> converter)
+        {
+            return Read(converter, null);
+        }
+
+        private T Read<T>(Func<string, T> converter, Type type = null)
+        {
+            string readValue = Value;
+            
+            Type targetType = type ?? typeof(T);
+            if (targetType == typeof(bool) && (String.IsNullOrWhiteSpace(Value) || Value.StartsWith("-") || Value.StartsWith("/")))
+                readValue = "true";
+
+            return converter(readValue);
+        }
+    }
+}
